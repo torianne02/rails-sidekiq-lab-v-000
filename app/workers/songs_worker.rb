88860eafcf6b6d938dabf6bb44b2dc69551163a6
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-09 18:39:03 +0000

    automatically backed up by learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -5,6 +5,6 @@ class SongsWorker
   def perform(songs_file)
     CSV.foreach(songs_file, headers: true) do |song|
       Song.create(title: song[0], artist_name: song[1])
-    end 
+    end
   end
 end
